2274adac79d2433bb46b723f1bb3f4470348cc43
Jason Han <hanjasn@gmail.com>
2024-12-24 06:30:17 -0800

    feat: add plugins

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+  'polirritmico/monokai-nightasty.nvim',
+  lazy = false,
+  priority = 1000,
+  keys = {
+    { '<leader>tt', '<Cmd>MonokaiToggleLight<CR>', desc = 'Monokai-Nightasty: Toggle dark/light theme.' },
+  },
+  ---@module "monokai-nightasty"
+  ---@type monokai.UserConfig
+  opts = {
+    dark_style_background = 'default', -- default | dark | transparent | #RRGGBB
+    light_style_background = 'default', -- default | dark | transparent | #RRGGBB
+    markdown_header_marks = true,
+    -- hl_styles = { comments = { italic = false } },
+    terminal_colors = function(colors)
+      return { fg = colors.fg_dark }
+    end,
+  },
+  config = function(_, opts)
+    vim.opt.cursorline = true -- Highlight line at the cursor position
+    vim.o.background = 'dark' -- Default to dark theme
+
+    require('monokai-nightasty').load(opts)
+  end,
+}
